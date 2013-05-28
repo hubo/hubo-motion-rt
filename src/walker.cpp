c373dbcc0c23724955d9e92457bdbf88e7d4f125
Michael Grey <mxgrey@gatech.edu>
2013-05-28 18:21:41 -0400

    Walker maybe finished

--- src/walker.cpp
+++ src/walker.cpp
@@ -20,6 +20,8 @@ const double nudgeIGain = 0.2;
 const double nudgeDGain = 0.0;
 */
 
+
+/* Set by balance param struct now
 const double jointSpaceTolerance = 0.075;
 
 const double hipDistance = 0.08843*2.0; // Distance between hip joints
@@ -46,54 +48,10 @@ const double fzMin = 10;
 const double fzMax = 50;
 
 const double flatteningGain = 0.02;
+*/
 
-// Stance Controllers
-void calibrateBoth( Hubo_Control &hubo );
-void horseStance( Hubo_Control &hubo );
-void craneStance( int side, Hubo_Control &hubo, double dt );
-void craneStance( int side, Vector6d swingVels, Hubo_Control &hubo, double dt );
+const double jointVelContTol = 6.0; // Joint trajectory velocity-based continuity tolerance
 
-
-// Horse Stance Quasi-Statics
-bool shiftToDistribution( int side, double distro, Hubo_Control &hubo, Balance_Monitor &trans, double dt );
-bool shiftToSide( int side, Hubo_Control &hubo, Balance_Monitor &trans, double dt );
-bool crouch( double height, Hubo_Control &hubo, double dt );
-
-
-// Crane Stance Quasi-Statics
-bool placeSwingFoot( int side, Eigen::Vector3d footPose, Hubo_Control &hubo, double dt );
-
-
-//const double compPitchGain = 0.0015;
-//const double shiftCompMultiplier = 1.0;
-//const double craneCompMultiplier = 1.0;
-
-
-// TODO: resurrect this later when roll ankle limits give us trouble
-//
-//const double craneHipPitchGain = 0.0*M_PI/180.0; // Not used
-//const double craneHipRollGain  = 0.05*M_PI/180.0; // Used when ankle roll reaches joint limits
-//const double craneAnkleRollThresh = 2.0*M_PI/180.0;
-
-//const double craneHipSafetyMargin = 2.5*M_PI/180.0; // Buffer to prevent leg collisions during crane stance
-//const double craneHipSafetyGain = 1.5;
-
-//const double swingVelLimit = 0.05;
-
-//const double rollAngleGain = 0.3*M_PI/180.0; // IMU feedback gain
-//const double craneAngleMultiplier = 1.0;
-
-//const double shiftGain = 0.0002; // Shift-to-side gain
-//const double shiftBalanceGain = 0.0015; // Gain to maintain balance while shifting
-//const double craneTransitionPercent = 98.0; // Percent of weight on swing foot before entering crane stance
-
-//const double crouchGain = 0.30;  // Leg-lifting gain
-
-//const double pcAccel = 0.4; // Nominal acceleration used for position control
-//const double vcAccel = 1.5; // Nominal acceleration used for velocity control
-
-//const double calKneeGain = 1.0;
-//const double calWeightGain = 0.001; // rad/sec per N
 
 double applyDeadband( double x ) {
 
@@ -107,78 +65,17 @@ double applyDeadband( double x ) {
 
 }
 
-
-void getLegNudge( Hubo_Control &hubo, Vector6d q, 
-		  zmp_traj_element_t elem, nudge_state_t &state, 
-		  int side, double dt )
-{
-    Eigen::Vector3d vel; vel.setZero();
-
-    double refAngleX=0, refAngleY=0;
-    // TODO: Calculate desired IMU angle
-
-    Eigen::Matrix3d R = ( Eigen::AngleAxisd(q(HY), Vector3d::UnitZ()) *
-			  Eigen::AngleAxisd(q(HR), Vector3d::UnitX()) *
-			  Eigen::AngleAxisd(q(HP), Vector3d::UnitY()) *
-			  Eigen::AngleAxisd(q(KN), Vector3d::UnitY()) *
-			  Eigen::AngleAxisd(q(AP), Vector3d::UnitY()) *
-			  Eigen::AngleAxisd(q(AR), Vector3d::UnitX()) ).toRotationMatrix();
-
-    /*
-    std::cout << "left MX: " << hubo.getLeftFootMx() << ", "
-	      << "desired: " << elem.torque[LEFT][0] << ", "
-	      << "diff: " << (hubo.getLeftFootMx() - elem.torque[LEFT][0]) << "\n";
-
-    std::cout << "left MY: " << hubo.getLeftFootMy() << ", "
-	      << "desired: " << elem.torque[LEFT][1] << ", "
-	      << "diff: " << (hubo.getLeftFootMy() - elem.torque[LEFT][1]) << "\n";
-
-    std::cout << "right MX: " << hubo.getRightFootMx() << ", "
-	      << "desired: " << elem.torque[RIGHT][0] << ", "
-	      << "diff: " << (hubo.getRightFootMx() - elem.torque[RIGHT][0]) << "\n";
-
-    std::cout << "right MY: " << hubo.getRightFootMy() << ", "
-	      << "desired: " << elem.torque[RIGHT][1] << ", "
-	      << "diff: " << (hubo.getRightFootMy() - elem.torque[RIGHT][1]) << "\n\n";
-    */
-
-    if( side==RIGHT )
-    {
-      vel(0) =  k_pos*applyDeadband( hubo.getRightFootMy() - elem.torque[RIGHT][1] );
-      vel(1) = -k_pos*applyDeadband( hubo.getRightFootMx() - elem.torque[RIGHT][0] );
-
-
-        state.nudge += R*vel*dt;
-
-        state.spin(0) += dt*k_theta_x*(hubo.getAngleX()-refAngleX);//-state.imu_offset(0));
-        state.spin(1) += dt*k_theta_y*(hubo.getAngleY()-refAngleY);//-state.imu_offset(0));
-
-        state.ankle_roll_compliance[RIGHT] += dt*k_tau*( hubo.getRightFootMx() );
-        state.ankle_pitch_compliance[RIGHT] += dt*k_tau*( hubo.getRightFootMy() );
-    }
-    else
-    {
-        vel(0) =  k_pos*applyDeadband( hubo.getLeftFootMy() - elem.torque[LEFT][1] );
-        vel(1) = -k_pos*applyDeadband( hubo.getLeftFootMx() - elem.torque[LEFT][0] );
-
-        state.nudge += R*vel*dt;
-
-        state.spin(0) += dt*k_theta_x*(hubo.getAngleX()-refAngleX);//-state.imu_offset(0));
-        state.spin(1) += dt*k_theta_y*(hubo.getAngleY()-refAngleY);//-state.imu_offset(0));
-
-        state.ankle_roll_compliance[LEFT] += dt*k_tau*( hubo.getLeftFootMx() );
-        state.ankle_pitch_compliance[LEFT] += dt*k_tau*( hubo.getLeftFootMy() );
-    }
-
-
-
-}
-
 static inline void clamp(double& x, double cap) {
   x = std::max(-cap, std::min(x, cap));
 }
 
 
+void executeTimeStep( Hubo_Control &hubo, zmp_traj_element_t &currentElem,
+            zmp_traj_element &prevElem, &gains, double dt );
+
+
+bool checkForNewTrajectory(zmp_traj_t &newTrajectory);
+bool validateNextTrajectory( zmp_traj_element_t &current, zmp_traj_element_t &next, double dt )
 
 
 void flattenFoot( Hubo_Control &hubo, zmp_traj_element_t &elem,
@@ -264,10 +161,14 @@ void complyKnee( Hubo_Control &hubo, zmp_traj_element_t &elem,
 }
 
 
+
+
+
 int main(int argc, char **argv)
 {
     Hubo_Control hubo("walker");
 
+    int timeIndex=0, nextTimeIndex=0, prevTimeIndex=0;
 
     nudge_state_t state;
     balance_gains_t gains;
@@ -283,26 +184,29 @@ int main(int argc, char **argv)
         fprintf( stderr, "%s (%d)\n", ach_result_to_string(r), (int)r );
     
     size_t fs;
-    zmp_traj_t trajectory;
-    memset( &trajectory, 0, sizeof(trajectory) );
+    zmp_traj_t prevTrajectory, currentTrajectory, nextTrajectory;
+    memset( &prevTrajectory, 0, sizeof(prevTrajectory) );
+    memset( &currentTrajectory, 0, sizeof(currentTrajectory) );
+    memset( &nextTrajectory, 0, sizeof(nextTrajectory) );
 
     do {
         struct timespec t;
         clock_gettime( ACH_DEFAULT_CLOCK, &t );
         t.tv_sec += 1;
-        ach_get( &zmp_chan, &trajectory, sizeof(trajectory), &fs, &t, ACH_O_WAIT | ACH_O_LAST );
+        ach_get( &zmp_chan, &currentTrajectory, sizeof(currentTrajectory), &fs, &t, ACH_O_WAIT | ACH_O_LAST );
     } while(!daemon_sig_quit && r==ACH_TIMEOUT);
+    daemon_assert( ACH_TIMEOUT != r, __LINE__ );
 
-    fprintf(stderr, "Count: %d\n", (int)trajectory.count);
-    for(int i=0; i<trajectory.count; i++)
-        fprintf(stdout, "%d: RHR %f\n", i, trajectory.traj[i].angles[RHR] );
+    fprintf(stderr, "Count: %d\n", (int)currentTrajectory.count);
+//    for(int i=0; i<currentTrajectory.count; i++)
+//        fprintf(stdout, "%d: RHR %f\n", i, currentTrajectory.traj[i].angles[RHR] );
 
     ach_get( &param_chan, &gains, sizeof(gains), &fs, NULL, ACH_O_LAST );
 
     hubo.update(true);
     for(int i=0; i<HUBO_JOINT_COUNT; i++)
     {
-        hubo.setJointAngle( i, trajectory.traj[0].angles[i] );
+        hubo.setJointAngle( i, currentTrajectory.traj[0].angles[i] );
         hubo.setJointNominalSpeed( i, 0.4 );
         hubo.setJointNominalAcceleration( i, 0.4 );
     }
@@ -312,88 +216,163 @@ int main(int argc, char **argv)
     hubo.setJointNominalSpeed( LKN, 0.8 );
     hubo.setJointNominalAcceleration( LKN, 0.8 );
 
-    hubo.setJointAngle( RSR, trajectory.traj[0].angles[RSR] + hubo.getJointAngleMax(RSR) );
-    hubo.setJointAngle( LSR, trajectory.traj[0].angles[LSR] + hubo.getJointAngleMin(LSR) );
+    hubo.setJointAngle( RSR, currentTrajectory.traj[0].angles[RSR] + hubo.getJointAngleMax(RSR) );
+    hubo.setJointAngle( LSR, currentTrajectory.traj[0].angles[LSR] + hubo.getJointAngleMin(LSR) );
 
     hubo.sendControls();
 
+    double maxWait = 15;
     
     double dt, time, stime; stime=hubo.getTime(); time=hubo.getTime();
     double norm = jointSpaceTolerance+1; // make sure this fails initially
-    while( !daemon_sig_quit && (norm > jointSpaceTolerance && time-stime < 15)) {
+    while( !daemon_sig_quit && (norm > jointSpaceTolerance && time-stime < maxWait)) {
       hubo.update(true);
       norm = 0;
       for(int i=0; i<HUBO_JOINT_COUNT; i++)
-          norm += (hubo.getJointAngleState( i )-trajectory.traj[0].angles[i])
-                   *(hubo.getJointAngleState( i )-trajectory.traj[0].angles[i]);
+          norm += (hubo.getJointAngleState( i )-currentTrajectory.traj[0].angles[i])
+                   *(hubo.getJointAngleState( i )-currentTrajectory.traj[0].angles[i]);
       norm = sqrt(norm);
       time = hubo.getTime();
     }
 
-    if( time-stime >= 15 )
-        fprintf(stderr, "Warning: could not reach the starting trajectory within 15 seconds");
+    if( time-stime >= maxWait )
+        fprintf(stderr, "Warning: could not reach the starting currentTrajectory within 15 seconds");
+    daemon_assert( time-stime < maxWait );
 
-
+    timeIndex = 1;
     while(!daemon_sig_quit)
     {
+        bool haveNewTrajectory = checkForNewTrajectory(nextTrajectory, haveNewTrajectory);
+        ach_get( &param_chan, &gains, sizeof(gains), &fs, NULL, ACH_O_LAST );
+        hubo.update(true);
+        dt = hubo.getTime() - time;
+        time = hubo.getTime();
 
-
-        fprintf(stdout, "%d\n", (int)trajectory.count);
-        for(int t=1; t<trajectory.count-1; t++)
+        if( timeIndex==0 )
         {
-            zmp_traj_element_t alteredElem;
-            memcpy( &alteredElem, &trajectory.traj[t], sizeof(alteredElem) );
-            
-            hubo.update(true);
-            dt = hubo.getTime() - time;
-            time = hubo.getTime();
-
-            ach_get( &param_chan, &gains, sizeof(gains), &fs, NULL, ACH_O_LAST );
-
-            flattenFoot( hubo, alteredElem, state, gains, dt );
-            straightenBack( hubo, alteredElem, state, gains, dt );
-            complyKnee( hubo, alteredElem, state, gains, dt );
-            //nudgeRefs( hubo, alteredElem, state, dt, hkin ); //vprev, verr, dt );
-            for(int i=0; i<HUBO_JOINT_COUNT; i++)
+            nextTimeIndex = timeIndex+1;
+            executeTimeStep( hubo, prevTrajectory.traj[prevTimeIndex],
+                                   currentTrajectory.traj[timeIndex],
+                                   currentTrajectory.traj[nextTimeIndex] );
+        }
+        else if( timeIndex == currentTrajectory.periodEndTick && haveNewTrajectory )
+        {
+            if( validateNextTrajectory( currentTrajectory.traj[timeIndex],
+                                        nextTrajectory.traj[0] ) )
             {
-                hubo.setJointAngle( i, alteredElem.angles[i] );
-                hubo.setJointNominalSpeed( i,
-                       (alteredElem.angles[i]-trajectory.traj[t-1].angles[i])*ZMP_TRAJ_FREQ_HZ );
-                double accel = ZMP_TRAJ_FREQ_HZ*ZMP_TRAJ_FREQ_HZ*(
-                                    trajectory.traj[t-1].angles[i]
-                                - 2*alteredElem.angles[i]
-                                +   trajectory.traj[t+1].angles[i] );
-                hubo.setJointNominalAcceleration( i, 10*accel );
+                nextTimeIndex = 0;
+                executeTimeStep( hubo, currentTrajectory.traj[prevTimeIndex],
+                                       currentTrajectory.traj[timeIndex],
+                                       nextTrajectory.traj[nextTimeIndex] );
+                
+                memcpy( &prevTrajectory, &currentTrajectory, sizeof(prevTrajectory) );
+                memcpy( &currentTrajectory, &nextTrajectory, sizeof(nextTrajectory) );
             }
+            else
+            {
+                fprintf(stderr, "WARNING: Discontinuous trajectory passed in. Walking to a stop.");
 
+                nextTimeIndex = timeIndex+1;
+                executeTimeStep( hubo, currentTrajectory.traj[prevTimeIndex],
+                                       currentTrajectory.traj[timeIndex],
+                                       currentTrajectory.traj[nextTimeIndex] );
+            }
+            haveNewTrajectory = false;
+        }
+        else if( timeIndex == currentTrajectory.periodEndTick && currentTrajectory.reuse )
+        {
+            nextTimeIndex = currentTrajectory.periodStartTick;
+            executeTimeStep( hubo, currentTrajectory.traj[prevTimeIndex],
+                                   currentTrajectory.traj[timeIndex],
+                                   currentTrajectory.traj[nextTimeIndex],
+                                   gains, dt );
+        }
+        else if( timeIndex < currentTrajectory.count-1 && !haveNewTrajectory )
+        {
+            nextTimeIndex = timeIndex+1;
+            executeTimeStep( hubo, currentTrajectory.traj[prevTimeIndex],
+                                   currentTrajectory.traj[timeIndex],
+                                   currentTrajectory.traj[nextTimeIndex],
+                                   gains, dt );
+        }
+        else if( timeIndex == currentTrajectory.count-1 && haveNewTrajectory )
+        {
+            if( validateNextTrajectory( currentTrajectory.traj[timeIndex],
+                                        nextTrajectory.traj[0] ) )
+            {
+                nextTimeIndex = 0;
+                executeTimeStep( hubo, currentTrajectory.traj[prevTimeIndex],
+                                       currentTrajectory.traj[timeIndex],
+                                       nextTrajectory.traj[nextTimeIndex] );
+                
+                memcpy( &prevTrajectory, &currentTrajectory, sizeof(prevTrajectory) );
+                memcpy( &currentTrajectory, &nextTrajectory, sizeof(nextTrajectory) );
+            }
+            else
+                fprintf(stderr, "WARNING: Discontinuous trajectory passed in. Discarding it.");
 
-            hubo.setJointAngle( RSR, alteredElem.angles[RSR] + hubo.getJointAngleMax(RSR) );
-            hubo.setJointAngle( LSR, alteredElem.angles[LSR] + hubo.getJointAngleMin(LSR) );
-
-            hubo.setJointAngleMin( LHR, alteredElem.angles[RHR] );
-            hubo.setJointAngleMax( RHR, alteredElem.angles[LHR] );
-            hubo.sendControls();
+            haveNewTrajectory = false;
         }
 
-        ach_status_t r = ACH_TIMEOUT;
 
-        do {
-            struct timespec t;
-            clock_gettime( ACH_DEFAULT_CLOCK, &t );
-            t.tv_sec += 1;
-            r = ach_get( &zmp_chan, &trajectory, sizeof(trajectory), &fs,
-                                      &t, ACH_O_WAIT | ACH_O_LAST );
-        } while(!daemon_sig_quit && r==ACH_TIMEOUT);
-
-
+        prevTimeIndex = timeIndex;
+        timeIndex = nextTimeIndex;
     }
 
+}
 
 
 
 
+bool checkForNewTrajectory(zmp_traj_t &newTrajectory, bool haveNewTrajAlready)
+{
+    size_t fs;
+    
+    ach_status_t r = ach_get( &zmp_chan, &newTrajectory, sizeof(newTrajectory), &fs, NULL, ACH_O_LAST );
+
+    if( ACH_OK==r || ACH_MISSED_FRAME==r )
+        return true;
+    else
+        return haveNewTrajAlready || false;
+
+}
+
+bool validateNextTrajectory( zmp_traj_element_t &current, zmp_traj_element_t &next, double dt )
+{
+    bool valid = true;
+    for(int i=0; i<HUBO_JOINT_COUNT; i++)
+        if( fabs(next.angles[i]-current.angles[i])/fabs(dt) > fabs(jointVelContTol) )
+            valid = false;
+    return valid;
+}
 
 
+void executeTimeStep( Hubo_Control &hubo, zmp_traj_element_t &prevElem,
+            zmp_traj_element_t &currentElem, zmp_traj_element &nextElem,
+            &gains, double dt )
+{
+        flattenFoot( hubo, currentElem, state, gains, dt );
+        straightenBack( hubo, currentElem, state, gains, dt );
+        complyKnee( hubo, currentElem, state, gains, dt );
+        //nudgeRefs( hubo, currentElem, state, dt, hkin ); //vprev, verr, dt );
+
+        for(int i=0; i<HUBO_JOINT_COUNT; i++)
+        {
+            hubo.setJointAngle( i, currentElem.angles[i] );
+            hubo.setJointNominalSpeed( i,
+                   (currentElem.angles[i]-previousElem.angles[i])*ZMP_TRAJ_FREQ_HZ );
+            double accel = ZMP_TRAJ_FREQ_HZ*ZMP_TRAJ_FREQ_HZ*(
+                                previousElem.angles[i]
+                            - 2*currentElem.angles[i]
+                            +   nextElem.angles[i] );
+            hubo.setJointNominalAcceleration( i, 10*accel );
+        }
 
 
+        hubo.setJointAngle( RSR, currentElem.angles[RSR] + hubo.getJointAngleMax(RSR) );
+        hubo.setJointAngle( LSR, currentElem.angles[LSR] + hubo.getJointAngleMin(LSR) );
+
+        hubo.setJointAngleMin( LHR, currentElem.angles[RHR] );
+        hubo.setJointAngleMax( RHR, currentElem.angles[LHR] );
+        hubo.sendControls();
 }
