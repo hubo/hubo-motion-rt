5749e0da0b45faabf95566e8413ecd954b1e2631
Pete <pete.vieira@gmail.com>
2013-08-26 17:32:12 -0400

    set to daemonized

--- src/balance-daemon.cpp
+++ src/balance-daemon.cpp
@@ -78,10 +78,10 @@ void moveHips(Hubo_Control &hubo, DrcHuboKin &kin, std::vector<LegVector, Eigen:
 
 int main(int argc, char **argv)
 {
-    //Hubo_Control hubo("balance-daemon", 35);
-    DrcHuboKin kin;
+    Hubo_Control hubo("balance-daemon", 35);
+    //Hubo_Control hubo;
 
-    Hubo_Control hubo;
+    DrcHuboKin kin;
 
     hubo.storeAllDefaults();
 
