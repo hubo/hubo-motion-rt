7c2af5e460c46123c8314815acc6ade6cc705954
Pete <pete.vieira@gmail.com>
2013-09-06 08:45:53 -0400

    added keyboard input header for imp-test

---
+++ include/KeyboardInput.h
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <termio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+static int tty_unbuffered(int);     
+static void tty_atexit(void);
+static int tty_reset(int);
+static void keyboard_init();
+
+static struct termios save_termios;
+static int  ttysavefd = -1;
+
+static int
+tty_unbuffered(int fd)      /* put terminal into a raw mode */
+{
+    struct termios  buf;
+
+    if (tcgetattr(fd, &buf) < 0)
+        return(-1);
+        
+    save_termios = buf; /* structure copy */
+
+    /* echo off, canonical mode off */
+    buf.c_lflag &= ~(ECHO | ICANON);
+
+    /* 1 byte at a time, no timer */
+    buf.c_cc[VMIN] = 1;
+    buf.c_cc[VTIME] = 0;
+    if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
+        return(-1);
+
+    ttysavefd = fd;
+    return(0);
+}
+
+static int
+tty_reset(int fd)       /* restore terminal's mode */
+{
+    if (tcsetattr(fd, TCSAFLUSH, &save_termios) < 0)
+        return(-1);
+    return(0);
+}
+
+static void
+tty_atexit(void)        /* can be set up by atexit(tty_atexit) */
+{
+    if (ttysavefd >= 0)
+        tty_reset(ttysavefd);
+}
+
+static void
+keyboard_init()
+{
+   /* make stdin unbuffered */
+    if (tty_unbuffered(STDIN_FILENO) < 0) {
+        std::cout << "Set tty unbuffered error" << std::endl;
+        exit(1);
+    }
+
+    atexit(tty_atexit);
+
+    /* nonblock I/O */
+    int flags;
+    if ( (flags = fcntl(STDIN_FILENO, F_GETFL, 0)) == 1) {
+        perror("fcntl get flag error");
+        exit(1);
+    }
+    if (fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl set flag error");
+        exit(1);
+    }
+}
+
