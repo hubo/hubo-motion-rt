5f0807bfc4c68decb16f7a1aa412aaff13de4bf6
M.X. Grey <mxgrey@gatech.edu>
2013-10-09 11:47:03 -0400

    Added IK Tester

--- src/manipTest.cpp
+++ src/manipTest.cpp
@@ -62,10 +62,71 @@ int main(int argc, char **argv)
 
     DrcHuboKin kin;
     
-    kin.linkage("RightLeg").printInfo();
+//    kin.linkage("RightLeg").printInfo();
+    LegVector q, targetVals; q.setZero(); targetVals.setZero();
+
+    kin.updateLegJoints(LEFT, q);
+
+    TRANSFORM B = kin.legFK(LEFT);
+
+//    cout << "Before: " << q.transpose() << endl;
+//    kin.legIK(LEFT, q, B);
+//    cout << "After:  " << q.transpose() << endl;
+//    q.setZero();
+
+//    q(KN) = 0.4;
+//    kin.updateLegJoints(LEFT, q);
+//    B = kin.legFK(LEFT);
+//    cout << "Before: " << q.transpose() << endl;
+//    kin.legIK(LEFT, q, B);
+//    cout << "After:  " << q.transpose() << endl;
+
+    string limb = "LeftLeg";
+
+    cout << kin.joint("RHR").min() << "\t" << kin.joint("RHR").max();
+    srand(time(NULL));
+
+    int tests = 1000;
+    int resolution = 1000;
+    int good = 0;
+
+    for(int k=0; k<tests; k++)
+    {
+        for(int i=0; i<6; i++)
+        {
+            int randomVal = rand();
+            targetVals(i) = ((double)(randomVal%resolution))/((double)resolution-1)
+                    *(kin.linkage(limb).joint(i).max() - kin.linkage(limb).joint(i).min())
+                    + kin.linkage(limb).joint(i).min();
+
+            randomVal = rand();
+
+            q(i) = ((double)(randomVal%resolution))/((double)resolution-1)
+                                *(kin.linkage(limb).joint(i).max() - kin.linkage(limb).joint(i).min())
+                                + kin.linkage(limb).joint(i).min();
+        }
+
+        kin.updateLegJoints(LEFT, targetVals);
+        B = kin.legFK(LEFT);
+
+        kin.updateLegJoints(LEFT, q);
+        kin.legIK(LEFT, q, B);
+        kin.updateLegJoints(LEFT, q);
+
+        if( (targetVals-q).norm() > 1e-12 )
+        {
+            cout << "Target: " << B.matrix() << endl << endl;
+            cout << "Actual: " << kin.legFK(LEFT).matrix() << endl << endl;
+        }
+        else
+            good++;
+    }
+
+    cout << "Good: " << good << endl;
 
 
-    cout << "Trunk Left:" << endl << kin.linkage("LeftLeg").joint(1).respectToRobot().matrix() << endl << endl;
 
-    cout << "Trunk Right:" << endl << kin.linkage("RightLeg").joint(1).respectToRobot().matrix() << endl;
+//    cout << "Trunk Left:" << endl << kin.linkage("LeftLeg").joint(1).respectToRobot().matrix() << endl << endl;
+
+//    cout << "Trunk Right:" << endl << kin.linkage("RightLeg").joint(1).respectToRobot().matrix() << endl;
 }
